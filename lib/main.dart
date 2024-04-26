4fccca155b9eafd88528b7a2e8b7a256a4f2e225
Satya0421 <mathukiyameet135520@gmail.com>
2024-04-26 11:19:43 +0530

    --> Create login screen and search page with api integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:impero_task/screen/home/category_scr.dart';
-
-void main() {
+import 'package:get_storage/get_storage.dart';
+import 'package:impero_task/screen/login/login_ctr.dart';
+import 'package:impero_task/screen/login/login_scr.dart';
+import 'package:impero_task/screen/searchscreen/search_scr.dart';
+late GetStorage box;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+   await GetStorage().initStorage;
+  box = GetStorage();
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Get.put(LoginPageController());
+
     return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
@@ -18,7 +29,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: CeramicScr(),
+      home:
+      box.read('isLoggedIn') ?SearchScreen() :
+      LoginPage(),
     );
   }
 }
